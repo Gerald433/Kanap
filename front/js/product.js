9a63be6caf61571dee45d950dc07ae945ebf4341
Gérald Gaillard <gerald8142@gmail.com>
2023-01-16 18:44:00 +0100

    improve page product, processing of products already added to the basket

--- front/js/product.js
+++ front/js/product.js
@@ -5,102 +5,117 @@ const urlParams = new URLSearchParams(myKeysValues);
 const kanapId = urlParams.get('id');
 
 
-fetch ("http://localhost:3000/api/products/" + kanapId)
-.then(res=>{
-    
-    return res.json()
+// panier vide
+let basket = [];
 
-})
-.then(kanape=>{
-   
-    const image = document.createElement("img")
-    
-    image.src = kanape.imageUrl
-    
-    image.alt = kanape.altTxt
-    
-    const divContain = document.getElementsByClassName("item__img")
-   
-    divContain[0].appendChild(image)
-    
-    console.log("dans le then")
-
-    const titleH1 = document.getElementById("title")
-    titleH1.textContent = kanape["name"]
-
-    const price = document.getElementById("price")
-    price.textContent = kanape["price"]
-
-    const description = document.getElementById("description")
-    description.textContent = kanape["description"]
-
-    // const colors = document.getElementsByAttributeName("value")
-    // colors.textContent = kanape["colors"["0","1","2"]]
-
-
-    for(i=0; i<kanape[0["".length]]; i++){
-       if ([i] !== ""){
-
-        console.log("ca passe")
-
-        let colorOption = document.createElement("option")
-        colorOption.textContent = kanape["colors"[i]]
-        let eltOption = document.getElementById("colors")
-        eltOption.appendChild(colorOption)
-       }
-    }
+//Si la clef "basket" est trouvé dans le LS, alors on stock cette valeur
+if (localStorage.getItem("basket") !== null) {
+    basket = JSON.parse(localStorage.getItem("basket"))
+}
 
 
 
-    
+fetch("http://localhost:3000/api/products/" + kanapId)
+    .then(res => {
+
+        return res.json()
+
+    })
+    .then(kanape => {
+
+        const image = document.createElement("img")
+        image.src = kanape.imageUrl
+        image.alt = kanape.altTxt
+
+        const divContain = document.getElementsByClassName("item__img")
+        divContain[0].appendChild(image)
+
+        const titleH1 = document.getElementById("title")
+        titleH1.textContent = kanape["name"]
+
+        const price = document.getElementById("price")
+        price.textContent = kanape["price"]
+
+        const description = document.getElementById("description")
+        description.textContent = kanape["description"]
+
+
+
+        //  Boucle permettant de récupérer les différentes couleurs et de les intégrer comme enfants 
+        for (let i = 0; i < kanape["colors"].length; i++) {
+
+            let colorOption = document.createElement("option")
+            colorOption.textContent = kanape["colors"][i]
+            let eltOption = document.getElementById("colors")
+            eltOption.appendChild(colorOption)
+
+        }
+
+        // Ecoute au click du bouton
+        const btnAddCart = document.getElementById("addToCart")
+        btnAddCart.addEventListener("click", function () {
+
+            // Fonction déclenchée suite au click...
+
+            // Je récupère les éléments du dom pour les manipuler
+            const selColor = document.getElementById("colors")
+            const inpQuant = document.getElementById("quantity")
+
+
+
+            // 2 Erreurs possibles à traiter: 
+
+            // 1er - Si Couleur non renseignée
+            if (selColor.value === "") {
+                alert("Veuillez renseigner la couleur")
+            }
+
+
+            // 2eme -  Si Quantité non renseignée (traduction de string en number avec parseInt)
+            if (parseInt(inpQuant.value) <= 0) {
+                alert("Veuillez renseigner la quantité")
+            }
+
+
+            // Objet contenant les éléments à passer au local storage
+            const produit = {
+                id: kanapId,
+                color: selColor.value,
+                quantity: Number(inpQuant.value)
+            }
+
+            // la valeur sera push dans basket
+           
+            let productFound
+            for (let i = 0; i < basket.length; i++) {
+               if(basket[i].id === produit.id && basket[i].color === produit.color ){
+                productFound = basket[i]
+               }
+                
+            }
+            if(productFound === undefined){
+                basket.push(produit)
+            }else{
+                productFound.quantity += produit.quantity 
+            }
+
+            localStorage.setItem("basket", JSON.stringify(basket))
+
+
+
+        })
+
+
+    })
+    .catch(err => {
+        console.log("dans le catch")
+        console.error(err)
+    })
 
 
 
 
 
 
-
-    
-})
-.catch(err=>{
-    console.log("dans le catch")
-    console.log(err)
-})
-
-
-
-
-
-
-
-
-
-// let url = new URL(response);
-
-// let id = url.searchParams.get("id");
-
-
-// console.log ("id")
-
-// Récupérer le nom du produit
-
-
-
-// Récupérer le prix du produit
-
-
-// Récupérer la description du produit
-
-
-// Récupérer les couleurs du produit.
-
-
-// function displayImg(img){
-//     const templateElt = document.getElementById("")
-//     const cloneElt = document.importNode(templateElt.content, true)
-
-//     cloneElt.getElementById("item__img").href += "?id=" + img.id
-
-// }
 
 
