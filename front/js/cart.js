f2ea9d20cbe438a2cab89158b5d2163f778bceca
Gérald Gaillard <gerald8142@gmail.com>
2023-02-01 18:55:42 +0100

    display kanapé, add events, begin form

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,102 +1,62 @@
-// rechercher les canapés
+
+//-------------------------------------------------
+// init
+//-------------------------------------------------
 const REPONSE = await fetch("http://localhost:3000/api/products")
 let canapes = await REPONSE.json()
-
-
-
 console.log(canapes)
-/*
-
-<h1>Votre panier</h1>
-<section class="cart">
-  <section id="cart__items">
-
-
-  <!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-    <div class="cart__item__img">
-      <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-    </div>
-    <div class="cart__item__content">
-      <div class="cart__item__content__description">
-        <h2>Nom du produit</h2>
-        <p>Vert</p>
-        <p>42,00 €</p>
-      </div>
-      <div class="cart__item__content__settings">
-        <div class="cart__item__content__settings__quantity">
-          <p>Qté : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-        </div>
-        <div class="cart__item__content__settings__delete">
-          <p class="deleteItem">Supprimer</p>
-        </div>
-      </div>
-    </div>
-    </article> -->
-    */
-
-
-
-
-
-// 0: 
-// altTxt: "Photo d'un canapé bleu, deux places"
-// colors: (3) ['Blue', 'White', 'Black']
-// description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-// imageUrl: "http://localhost:3000/images/kanap01.jpeg"
-// name: "Kanap Sinopé"
-// price: 1849
-// _id: "107fb5b75607497b96722bda5b504926"
-
+const itemParent = document.querySelector("#cart__items")
 
 let listObjets = JSON.parse(localStorage.basket)
-
 let sum = 0
 let mult = 0
-for (let i = 0; i < listObjets.length; i++) {
 
+
+//-------------------------------------------------
+// Création du panier
+//-------------------------------------------------
+// inspection du localStorage avec boucle for
+for (let i = 0; i < listObjets.length; i++) {
   let objetId = listObjets[i].id
   let objetColor = listObjets[i].color
   let objetQuantity = listObjets[i].quantity
 
-
+  // comparaison entre tableau du localStorage, et la liste d'objets de la réponse fetch
   let elementFound
   for (let j = 0; j < canapes.length; j++) {
-    console.log(listObjets[i].id, canapes[j]._id)
+    // si l'id d'un objet est strictement égal à l'id d'un canapé, alors elementFound stock les renseignements de l'objet canapé
     if (listObjets[i].id === canapes[j]._id) {
       elementFound = canapes[j]
+      break
     }
-  } console.log(elementFound)
+  }
+  // Si elementFound est défini, on crée la structure html attendue
   if (elementFound !== undefined) {
-    // Création de l'article 
 
+    // Création de l'article 
     const articleCartItem = document.createElement("article")
-    const itemParent = document.querySelector("#cart__items")
+
     itemParent.appendChild(articleCartItem)
+
     articleCartItem.className = "cart__item"
     articleCartItem.setAttribute("data-id", objetId)
     articleCartItem.setAttribute("data-color", objetColor)
 
-
     // Création de la div enfant 
-
     const divImg = document.createElement("div")
     articleCartItem.appendChild(divImg)
     divImg.className = "cart__item__img"
 
     // Création de l'image 
-
     const image = document.createElement("img")
     divImg.appendChild(image)
     image.src = elementFound.imageUrl
     image.alt = elementFound.altTxt
 
-
     // création de la div  cart__item__content
     const general = document.createElement("div")
     general.className = "cart__item__content"
     articleCartItem.appendChild(general)
-
 
     // création de la div cart__item__content__description
     const nameColorPrice = document.createElement("div")
@@ -104,8 +64,6 @@ for (let i = 0; i < listObjets.length; i++) {
     general.appendChild(nameColorPrice)
 
     // création nom du produit
-
-
     const name = document.createElement("h2")
     name.textContent = elementFound.name
     nameColorPrice.appendChild(name)
@@ -120,23 +78,21 @@ for (let i = 0; i < listObjets.length; i++) {
     price.textContent = elementFound.price + " €"
     nameColorPrice.appendChild(price)
 
-    // création de la div cart__item__content__settings
-    const cart__item__content__settings = document.createElement("div")
-    cart__item__content__settings.className = "cart__item__content__settings"
-    general.appendChild(cart__item__content__settings)
+    // création de la div divSettings
+    const divSettings = document.createElement("div")
+    divSettings.className = "cart__item__content__settings"
+    general.appendChild(divSettings)
 
-    // création de la div cart__item__content__settings__quantity
-    const cart__item__content__settings__quantity = document.createElement("div")
-    cart__item__content__settings__quantity.className = "cart__item__content__settings__quantity"
-    cart__item__content__settings.appendChild(cart__item__content__settings__quantity)
+    // création de la div divQuantity
+    const divQuantity = document.createElement("div")
+    divQuantity.className = "cart__item__content__settings__quantity"
+    divSettings.appendChild(divQuantity)
 
     // création p quantity
     const p_quantity = document.createElement("p")
     p_quantity.textContent = "Qté : "
-    cart__item__content__settings__quantity.appendChild(p_quantity)
+    divQuantity.appendChild(p_quantity)
 
-
-    // input type = "number" class="itemQuantity" name = "itemQuantity" min = "1" max = "100" value = "42" >
     // création input quantity
     const i_quantity = document.createElement("input")
     i_quantity.setAttribute("type", "number")
@@ -145,107 +101,284 @@ for (let i = 0; i < listObjets.length; i++) {
     i_quantity.setAttribute("max", "100")
     i_quantity.setAttribute("value", objetQuantity)
     i_quantity.className = "itemQuantity"
-    cart__item__content__settings__quantity.appendChild(i_quantity)
 
+    divQuantity.appendChild(i_quantity)
 
-    // parent = cart__item__content__settings
-    // // <div class="cart__item__content__settings__delete">
-    // <p class="deleteItem">Supprimer</p>
-    // </div>
-
-    // création de la div cart__item__content__settings__delete
-    const cart__item__content__settings__delete = document.createElement("div")
-    cart__item__content__settings__delete.className = "cart__item__content__settings__delete"
-    cart__item__content__settings.appendChild(cart__item__content__settings__delete)
-
-
+    // création de la div divDelete
+    const divDelete = document.createElement("div")
+    divDelete.className = "cart__item__content__settings__delete"
+    divSettings.appendChild(divDelete)
 
     // création <p class="deleteItem">Supprimer</p>
     const deleteItem = document.createElement("p")
     deleteItem.className = "deleteItem"
     deleteItem.textContent = "Supprimer"
-    cart__item__content__settings.appendChild(deleteItem)
+
+    divSettings.appendChild(deleteItem)
+    //-------------------------------------------------
+    //                   Fin du panier
+    //-------------------------------------------------
 
 
 
 
 
-    //  <p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>
-    // ///////////////////////////////////////////////////////
+    //-------------------------------------------------
+    // Détection du changement de la quantité
+    //-------------------------------------------------
+    i_quantity.addEventListener('change', function (event) {
+      let index = findIndexOfCanape(articleCartItem)
+      listObjets[index].quantity = event.target.valueAsNumber
+      localStorage.setItem("basket", JSON.stringify(listObjets))
 
-   
-  
+      recalculTotalQuantity()
+      recalculTotalPrice()
 
-    
+    })
+
+
+    //-------------------------------------------------
+    // détection delete
+    //-------------------------------------------------
+    deleteItem.addEventListener("click", function () {
+      itemParent.removeChild(articleCartItem)
+      let index = findIndexOfCanape(articleCartItem)
+      listObjets.splice(index, 1)
+      localStorage.setItem("basket", JSON.stringify(listObjets))
+      console.log(listObjets)
+
+      recalculTotalQuantity()
+      recalculTotalPrice()
+    })
+
+
+
+    //-------------------------------------------------
+    // Incrémentation des totaux
+    //-------------------------------------------------
     sum += objetQuantity;
-
     mult += elementFound.price * objetQuantity
-
-
-
-
-
-
-
-
-    //  quant du LS * prix de la reponse fetch  
 
   }
 
 
+  //-------------------------------------------------
+  //Injection des totaux
+  //-------------------------------------------------
   let placement = document.querySelector("#totalQuantity")
   placement.textContent = sum
-
-
-  console.log(sum);
-
-
-
-
   let placementBis = document.querySelector("#totalPrice")
-  placementBis.textContent = mult 
-
-
-  console.log(mult);
-
-
-
-
-
-
-
-
-  // function createElement(){
-
-  // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // console.log(localStorage.basket.length)
-
-  // function callItemFromStorage(){
-
-  // }
-
-
-
-
-
+  placementBis.textContent = mult
 }
 
 
 
+//-------------------------------------------------
+// Mes fonctions
+//-------------------------------------------------
+
+function recalculTotalQuantity() {
+
+  let sum = 0
+  for (let i = 0; i < listObjets.length; i++) {
+
+    let quantity = listObjets[i].quantity
+
+    sum += quantity;
+
+
+
+  }
+  let placement = document.querySelector("#totalQuantity")
+  placement.textContent = sum
+}
+
+function recalculTotalPrice() {
+  let toto = 0
+  let mult = 0
+  for (let i = 0; i < listObjets.length; i++) {
+    let elementFound
+    for (let j = 0; j < canapes.length; j++) {
+      // si l'id d'un objet est strictement égal à l'id d'un canapé, alors elementFound stock les renseignements de l'objet canapé
+      if (listObjets[i].id === canapes[j]._id) {
+        elementFound = canapes[j]
+        break
+      }
+    }
+
+    let quantity = listObjets[i].quantity
+    toto += (quantity * elementFound.price)
+
+    console.log(quantity)
+    console.log(elementFound.price)
+
+  }
+  let placementBis = document.querySelector("#totalPrice")
+  placementBis.textContent = toto
+}
+
+function findIndexOfCanape(articleCartItem) {
+  let index
+  for (let j = 0; j < listObjets.length; j++) {
+    if (listObjets[j].id === articleCartItem.getAttribute("data-id")) {
+      index = j
+      break
+    }
+
+
+  }
+  return index
+}
+
+
+
+//-------------------------------------------------
+// vérification de renseignements formulaire
+//-------------------------------------------------
+
+// Prénom
+let firstName = document.getElementById("firstName")
+
+firstName.addEventListener("input", function () {
+  let regexFirstName = /^[a-zàâäéèêëïîôöùûüÿç-]{1,15}$/i
+  let resultControlFirstName = regexFirstName.test(firstName.value)
+  let errorMessage = document.getElementById("firstNameErrorMsg")
+  if (resultControlFirstName === false) {
+    errorMessage.textContent = "Ce champ n'accepte que les caractères alphabétiques dans la limite de 1 à 15"
+    return
+  } else {
+    errorMessage.textContent = ""
+    return
+  }
+})
+
+// nom
+let lastName = document.getElementById("lastName")
+
+lastName.addEventListener("input", function () {
+  let regexLastName = /^[a-zàâäéèêëïîôöùûüÿç-]{1,15}$/i
+  let resultControlLastName = regexLastName.test(lastName.value)
+  let errorMessage = document.getElementById("lastNameErrorMsg")
+  if (resultControlLastName === false) {
+    errorMessage.textContent = "Ce champ n'accepte que les caractères alphabétiques dans la limite de 1 à 15"
+    return
+  } else {
+    errorMessage.textContent = ""
+    return
+  }
+})
+
+
+// adress
+let address = document.getElementById("address")
+
+address.addEventListener("input", function () {
+  let regexAddress = /^[a-z0-9-\sàâäéèêëïîôöùûüÿç]{1,50}$/i
+  let resultControlAddress = regexAddress.test(address.value)
+  let errorMessage = document.getElementById("addressErrorMsg")
+  if (resultControlAddress === false) {
+    errorMessage.textContent = "Ce champ n'accepte que les caractères alphanumériques dans la limite de 1 à 50"
+    return
+  } else {
+    errorMessage.textContent = ""
+    return
+  }
+})
+
+
+// city
+let city = document.getElementById("city")
+
+city.addEventListener("input", function () {
+  let regexCity = /^[a-z\sàâäéèêëïîôöùûüÿç-]{1,40}$/i
+  let resultControlCity = regexCity.test(city.value)
+  let errorMessage = document.getElementById("cityErrorMsg")
+  if (resultControlCity === false) {
+    errorMessage.textContent = "Ce champ n'accepte que les caractères alphabétiques dans la limite de 1 à 40"
+    return
+  } else {
+    errorMessage.textContent = ""
+    return
+  }
+})
+
+
+// Email
+let email = document.getElementById("email")
+
+email.addEventListener("input", function () {
+  let regexEmail = /^([a-z\d_\.-]+)@([a-z\d\.-]+)\.([a-z\.]{2,6})$/
+  let resultControlEmail = regexEmail.test(email.value)
+  let errorMessage = document.getElementById("emailErrorMsg")
+  if (resultControlEmail === false) {
+    errorMessage.textContent = "Ce champ doit contenir un '@' et n'accepte que les caractères alphabétiques minuscules et numériques dans la limite de 1 à 40"
+    return
+  } else {
+    errorMessage.textContent = ""
+    return
+  }
+})
+
+//-------------------------------------------------
+// Création d'un objet de contact
+//-------------------------------------------------
+
+
+
+/**
+ *
+ * Expects request to contain:
+ * contact: {
+ *   firstName: string,
+ *   lastName: string,
+ *   address: string,
+ *   city: string,
+ *   email: string
+ * }
+ * products: [string] <-- array of product _id
+ *
+ */
+
+
+
+// solution plus intéressante
+// products: listObjets.map((objet) =>objet.id)
+
+
+
+//-------------------------------------------------
+// Valider la commande
+//-------------------------------------------------
+
+
+let order = document.getElementById("order")
+
+order.addEventListener("click", function (event) {
+  event.preventDefault()
+  // 1ere etape vérifier que tous les input sont bons
+  // si c est ok envoyer le formulaire sinon afficher une alerte
+  let body = {
+    contact: {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      address: address.value,
+      city: city.value,
+      email: email.value,
+    },
+
+    products: listObjets.map(function (objet) {
+      return objet.id
+    })
+  }
+  console.log(body)
+})
+
+
+
+
+
+// chercher l'élément et le stocker dans une variable
+//
 
 
 
@@ -253,8 +386,24 @@ for (let i = 0; i < listObjets.length; i++) {
 
 
 
+    // ///////////////////////////////////
+    // TABLEAU RENSEIGNEMENTS CLIENTS ///
+    // //////////////////////////////////
+
+    // const arrayClient = []
+    // arrayId.push(document.getElementById("firstName"))
+    // arrayId.push(document.getElementById("lastName"))
+    // arrayId.push(document.getElementById("address"))
+    // arrayId.push(document.getElementById("city"))
+    // arrayId.push(document.getElementById("email"))
+
+
+    //   // console.log(arrayClient)
+
+    //   /////////////////////////////////////////////////////////////////////////
 
 
 
 
 
+    //   // input.addEventListener ("change" , reaction() {
